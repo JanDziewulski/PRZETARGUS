7327aec81de7b03a6294fd0adcac0c29c093a5aa
jdziewulski <jandziewulski92@gmail.com>
2020-11-12 18:12:23 +0100

    nip and mail table

--- src/frontend.py
+++ src/frontend.py
@@ -2,9 +2,16 @@ from tkinter import *
 from tkinter import filedialog
 import time
 # from tkinter import ttk
+from przetargus import PDFsearcher
+
+n_object = PDFsearcher()
+
+
+
 
 
 def browseFiles():
+    global filename
     filename = filedialog.askopenfilename(initialdir="/",
                                           title="Wybierz plik",
                                           filetypes=(("Plik pdf",
@@ -13,12 +20,31 @@ def browseFiles():
                                                       "*.*")))
     # Change label contents
     label_file_explorer.configure(text="Plik: " + filename)
+
     return filename
 
+def set_text(text):
+    e.delete(0,END)
+    e.insert(0,text)
+    return
+
+def start():
+    # global text_info
+    text = n_object.pdf_read(filename) # pobieranie warości z nazwy pliku
+    text_info = n_object.pdf_info(text) #pobieranie wartości pdf info z pliku
+    set_text(text_info) # wprowadzanie wartości do tabelki
+
 window = Tk()
-window.geometry('600x700')
+
 # window.maxsize(600,700)
 # window.minsize(600,700)
+
+e = Entry(window,width=10)
+e.grid(row =10, column =3)
+
+
+window.geometry('600x700')
+
 blank_space =" "
 current_time = time.strftime("%d-%m-%Y-%H:%M")
 window.title('PRZETARGUS 1.0.15' + 60* blank_space + 'DATA: {}'.format(current_time))
@@ -31,6 +57,11 @@ label_file_explorer = Label(window,
 
 l1 = Label(window, text = "URL:",width = 10)
 l2 = Label(window, text = "LUB",width = 10)
+
+# text_box = Text(window, width = 25, height = 2)
+# text_box.grid(row = 7, column = 3)
+# text_box.insert("end-1c", text_info)
+# text_box.configure(state='disabled')
 
 http_link=StringVar()
 e1=Entry(window,text='Podaj link', textvariable=http_link,
@@ -47,7 +78,10 @@ button_exit = Button(window,
                      command = exit,
                      width = 10)
 
-
+button_start = Button(window,
+                     text = "Start",
+                     command = start,
+                     width = 10)
 
 # list1=Listbox(window, height=6,width=35)
 # list1.grid(row=2,column=0,rowspan=6,columnspan=2)
@@ -65,10 +99,13 @@ button_exit = Button(window,
 
 
 # specifying rows and columns
-label_file_explorer.grid(column = 3, row = 1, pady=10)
+label_file_explorer.grid(column = 3, row = 1, pady=30)
 l1.grid(column = 2 , row = 3, padx=30)
 l2.grid(column = 3 , row = 2, padx=30)
 button_explore.grid(column = 2 , row = 1, padx=30)
 button_exit.grid(column = 3 , row =20, pady = 150)
-window.grid_columnconfigure(4, minsize=100)
+button_start.grid(column = 3 , row =20, pady = 100)
+# window.grid_columnconfigure(4, minsize=100)
+
+
 window.mainloop()
--- src/przetargus.py
+++ src/przetargus.py
@@ -13,124 +13,114 @@ import os
 import time
 from openpyxl import load_workbook
 # import frontend as fr
+import statistics
+from statistics import mode
+
+class PDFsearcher:
+
+    def pdf_read(self, file):
+        # pdf_data = fr.browseFiles()
+        fp = open(file, 'rb')
+        rsrcmgr = PDFResourceManager()
+        retstr = io.StringIO()
+        print(type(retstr))
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        page_no = 0
+        for pageNumber, page in enumerate(PDFPage.get_pages(fp)):
+            if pageNumber == page_no:
+                interpreter.process_page(page)
+
+                data = retstr.getvalue()
+
+            page_no += 1
+        return data
+
+#wyszukiwanie danych do okna dialogowego
+    def pdf_info(self, file):
+        mail = re.search(r'(\w+([@])\w+.\w+)|(\w+.\w+([@])\w+.\w+)', file)
+        nip = re.search(r'NIP(\W+\d{10})|NIP(\W+PL\d{10})|NIP(\W+PL\d{3}-\d{2,3}-\d{2}-\d{2,3})|NIP(\W+\d{3}-\d{2,3}-\d{2}-\d{2,3})', file)
+        return mail.group(), nip.group()
 
 
-# find_email_from_pdf = r'(\w+([@])\w+.\w+)|(\w+.\w+([@])\w+.\w+)'
-#find_nip_fpdf = r'NIP(\W+\d{10})|NIP(\W+PL\d{10})|NIP(\W+PL\d{3}-\d{2,3}-\d{2}-\d{2,3})|NIP(\W+\d{3}-\d{2,3}-\d{2}-\d{2,3})'
+    def text_recon(self, text, text_to_recognize):
+        """Function that checks the presence of text in a pdf file.
 
-def pdf_info(file):
-    fp = open(file, 'rb')
-    parser = PDFParser(fp)
-    doc = PDFDocument(parser)
-    print(doc.info)  # The "Info" metadata
+            Args:
+                text (str): Parameter imported from .xlsx file.
 
-def pfd_read(file):
-    # pdf_data = fr.browseFiles()
-    fp = open(file, 'rb')
-    rsrcmgr = PDFResourceManager()
-    retstr = io.StringIO()
-    print(type(retstr))
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    page_no = 0
-    for pageNumber, page in enumerate(PDFPage.get_pages(fp)):
-        if pageNumber == page_no:
-            interpreter.process_page(page)
+            Returns:
+                bool: The return value. True for success, False otherwise.
 
-            data = retstr.getvalue()
+            """
 
-        page_no += 1
+        """Loading and cleaning pdf file"""
+        # local_pdf_filename = "ogloszenie_12215.pdf"  # path to file
+        # pages = [7]  # page number
 
-    # directory = './przetarg/'
-    # filename = time.strftime('przetarg_' + "%Y%m%d-%H%M%S" + '.txt')
-    # file_path = os.path.join(directory, filename)
-    # if not os.path.isdir(directory):
-    #     os.mkdir(directory)
-    # file = open(file_path, "wb")
-    # file.write(data.encode('utf-8'))
-    # file.close()
+        # extracted_text = high_level.extract_text(local_pdf_filename, "", pages)
+        text = text.lower()
+        extracted_text_clean = re.sub('[^\s\d\w]', '', text)  # text clean
+        # print(extracted_text_clean)
 
-    return data
+        """Reading and converting searched text"""
+        reg_rext = text_to_recognize
+        reg_rext_cln = re.sub('[^\s\d\w]', '', reg_rext)  # cleaned text
+        text_to_list = reg_rext_cln.lower().split(' ')
+        # print(text_to_list)
 
+        """List with item paris"""
+        if len(text_to_list) % 2 == 0:
+            pair_list = [text_to_list[i] + ' ' + text_to_list[i + 1] for i in range(0, len(text_to_list), 2)]
+        else:
+            pair_list = [text_to_list[i] + ' ' + text_to_list[i + 1] for i in range(0, len(text_to_list) - 1, 2)]
 
-def text_recon(text, text_to_recognize):
-    """Function that checks the presence of text in a pdf file.
+        # print(pair_list)
 
-        Args:
-            text (str): Parameter imported from .xlsx file.
+        count = 0
+        for i in range(len(pair_list)):
+            founded_item = re.search(pair_list[i], extracted_text_clean)
+            try:
+                if founded_item.group() == pair_list[i]:
+                    count += 1
+            except BaseException:
+                pass
 
-        Returns:
-            bool: The return value. True for success, False otherwise.
+                # print('Nie znaleziono tego słowa')
+
+        c_ratio = count / len(pair_list)
+        if 1 >= c_ratio >= 0.8:
+            return True
+            # print('Rozpoznanie z pełnym Sukcesem')
+        elif 0.8 > c_ratio >= 0.5:
+            print('Sprawdź jeszcze ręcznie ')
+            return False
+        else:
+            return False
+            print('Współczynnik rozpoznania {}'.format(round(c_ratio, 2)))
+
+    def text_from_excel(self):
 
         """
+        emp_list[0][0] - opis funkcjonalności, np. Wbudowana baza uchwytów, imadła, uchwyty 3 szczękowe, łapy dociskowe (niedopuszczalne jest dostarczenie bazy osobno poza systemem),
+        emp_list[0][1] - nazwa programu, np. Edgecam
+        emp_list[0][2] - informacje dodatkowe, np. Nie spełniamy wymagań
 
-    """Loading and cleaning pdf file"""
-    # local_pdf_filename = "ogloszenie_12215.pdf"  # path to file
-    # pages = [7]  # page number
-
-    # extracted_text = high_level.extract_text(local_pdf_filename, "", pages)
-    text = text.lower()
-    extracted_text_clean = re.sub('[^\s\d\w]', '', text)  # text clean
-    # print(extracted_text_clean)
-
-    """Reading and converting searched text"""
-    reg_rext = text_to_recognize
-    reg_rext_cln = re.sub('[^\s\d\w]', '', reg_rext)  # cleaned text
-    text_to_list = reg_rext_cln.lower().split(' ')
-    # print(text_to_list)
-
-    """List with item paris"""
-    if len(text_to_list) % 2 == 0:
-        pair_list = [text_to_list[i] + ' ' + text_to_list[i + 1] for i in range(0, len(text_to_list), 2)]
-    else:
-        pair_list = [text_to_list[i] + ' ' + text_to_list[i + 1] for i in range(0, len(text_to_list) - 1, 2)]
-
-    # print(pair_list)
-
-    count = 0
-    for i in range(len(pair_list)):
-        founded_item = re.search(pair_list[i], extracted_text_clean)
-        try:
-            if founded_item.group() == pair_list[i]:
-                count += 1
-        except BaseException:
-            pass
-
-            # print('Nie znaleziono tego słowa')
-
-    c_ratio = count / len(pair_list)
-    if 1 >= c_ratio >= 0.8:
-        return True
-        # print('Rozpoznanie z pełnym Sukcesem')
-    elif 0.8 > c_ratio >= 0.5:
-        print('Sprawdź jeszcze ręcznie ')
-        return False
-    else:
-        return False
-        print('Współczynnik rozpoznania {}'.format(round(c_ratio, 2)))
-
-def text_from_excel():
-
-    """
-    emp_list[0][0] - opis funkcjonalności, np. Wbudowana baza uchwytów, imadła, uchwyty 3 szczękowe, łapy dociskowe (niedopuszczalne jest dostarczenie bazy osobno poza systemem),
-    emp_list[0][1] - nazwa programu, np. Edgecam
-    emp_list[0][2] - informacje dodatkowe, np. Nie spełniamy wymagań
-
-    :return:
-    """
-    global iter_num
-    workbook = load_workbook(filename="Wymagania przetargowe.xlsx")
-    sheet = workbook.active
-    iter_num = sheet.max_row
-    apn_list = []
-    for value in sheet.iter_rows(min_row=2, min_col=2, max_col=4, values_only=True):
-        apn_list.append(value)
-        # print(value)
-        # text_recon(value)
-    return apn_list
-    # print(apn_list[1][0])
+        :return:
+        """
+        global iter_num
+        workbook = load_workbook(filename="Wymagania przetargowe.xlsx")
+        sheet = workbook.active
+        iter_num = sheet.max_row
+        apn_list = []
+        for value in sheet.iter_rows(min_row=2, min_col=2, max_col=4, values_only=True):
+            apn_list.append(value)
+            # print(value)
+            # text_recon(value)
+        return apn_list
+        # print(apn_list[1][0])
 
 
 
@@ -138,19 +128,23 @@ def text_from_excel():
 # text_recon()
 if __name__ == '__main__':
     # imput_data= pfd_read('ogloszenie_12215.pdf')
+    c = Przetargus()
 
-    imput_data= pfd_read('ogloszenie_12215.pdf')
+    imput_data= c.pdf_read('ogloszenie_12215.pdf')
     # text_recon(imput_data)
     # print(imput_data)
     # text_from_excel()
-    excel_list = text_from_excel()
+    excel_list = c.text_from_excel()
+    print(c.pdf_info(imput_data))
+    program_list = []
     for i in range(iter_num - 1):
-        val = text_recon(imput_data, excel_list[i][0])
+        val = c.text_recon(imput_data, excel_list[i][0])
         if val is True:
+            program_list.append(excel_list[i][1])
             print(excel_list[i][1:])
         else:
             print('Nie rozpoznano!')
-        # print(excel_list[i][0])
+    print(mode(program_list))
 
 
 
